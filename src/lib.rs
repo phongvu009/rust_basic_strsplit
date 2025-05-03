a4d8847e8746f25af3f9f7ebfda89720cbbc714f
phong vu <kimphong90@hotmail.com>
2025-05-03 12:41:12 -0500

    implement trait Iterator

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,19 @@
 #![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
 
-#create struct
+//create struct
 pub struct StrSplit {}
 
-#add function
+//add function
 impl StrSplit{
-    pub fn new(haystack: &str)
+    pub fn new(haystack: &str, delimiter: &str) -> Self{
+
+    }
+}
+
+//implement trait
+impl Iterator for StrSplit{
+    //def a type
+    type Item=&str;
+    //return Some() or None
+    fn next(&mut self) ->Option(Self::Item)
 }
